9fbeb26e8e2bb16a9a3f4fec3e3d5544d92256d6
graying <graying@gmail.com>
2017-03-06 00:40:58 -0800

    templates used

--- dj1/dj1/views.py
+++ dj1/dj1/views.py
@@ -1,20 +1,27 @@
+import datetime, os
+
 from django import template
 from django.http import HttpResponse
-import datetime
+from django.template.loader import get_template
+
 
 def hello(request):
     return HttpResponse("Hello world!......")
+
 
 def date_time(request):
     now = datetime.datetime.now()
     html = "<html><body>Haha! It is now %s.</body></html>" % now
     return HttpResponse(html)
 
+
 def smile(request):
     html = "<html><h1>Haha! smile</h1></html>"
     return HttpResponse(html)
 
+
 def t1test(request):
-    t = template.Template("this is a template named {{ temp_name }}.")
-    c = template.Context({'temp_name': 'Eddys_Template'})
+    t = get_template("t1test.tplt")
+    c = template.Context({'temp_name': 'Eddys_Template', 'number': '12'})
+
     return HttpResponse(t.render(c))
